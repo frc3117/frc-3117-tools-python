5b4d594a2be31f3f55798fd6b5485af01db17127
mickjosh <felix_roux@hotmail.com>
2024-03-09 11:42:43 -0500

    Fix CSV Logger

--- frctools/logging/csv_logger.py
+++ frctools/logging/csv_logger.py
@@ -4,6 +4,13 @@ from datetime import datetime
 import os
 
 
+def stringify(data):
+    if isinstance(data, float):
+        return str(round(data, 3))
+
+    return str(data)
+
+
 class CSVLogger:
     __LOG_FOLDER__ = '/home/lvuser/pythonLog'
 
@@ -23,7 +30,7 @@ class CSVLogger:
 
         now = datetime.now()
 
-        self.__file = open(f'{CSVLogger.__LOG_FOLDER__}/{now.strftime("%m-%d-%y_%H:%M:%S")}_{self.file_name}.csv', 'w')
+        self.__file = open(f'{CSVLogger.__LOG_FOLDER__}/{now.strftime("%m-%d-%y_%H:%M:%S")}_{self.file_name}.csv', 'a', buffering=0)
         self.__file.write('\t'.join([d[0] for d in self.data]) + '\n')
 
         self.__last_flush = Timer.get_current_time()
@@ -44,10 +51,12 @@ class CSVLogger:
 
     def __log_loop__(self):
         while True:
-            yield None
-            self.__file.write('\t'.join([str(d[1]()) for d in self.data]) + '\n')
+            self.__file.write('\t'.join([stringify(d[1]()) for d in self.data]) + '\n')
             if Timer.get_elapsed(self.__last_flush) >= 0.25:
                 self.__file.flush()
+                self.__last_flush = Timer.get_current_time()
+
+            yield None
 
     def add_data(self, name: str, data_source):
         self.data.append((name, data_source))
